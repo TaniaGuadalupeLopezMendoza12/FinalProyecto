c19d5b32b89a3b95bed52604774e5909ac736c71
Tania <tani.02g@gmai.com>
2025-07-01 21:54:50 -0600

    Cambio temporal para forzar renombrado

---
+++ dist/src/temp_dummy.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Point2D = void 0;
+// Point2D.java: Class for points in logical coordinates.
+// Contains 3D object data
+var Point2D = /** @class */ (function () {
+    function Point2D(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    return Point2D;
+}());
+exports.Point2D = Point2D;
