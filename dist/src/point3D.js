1ff2c2f1c166830396db3415658242acc52aab90
Tania <tani.02g@gmai.com>
2025-07-01 20:29:16 -0600

    Corrijo nombre de archivo para coincidir con importación

--- dist/src/point3D.js
+++
@@ -1,9 +0,0 @@
-var Point3D = /** @class */ (function () {
-    function Point3D(x, y, z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-    }
-    return Point3D;
-}());
-export { Point3D };
