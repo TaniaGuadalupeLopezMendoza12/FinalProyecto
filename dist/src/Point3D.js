adfc5a87db8b696c2bab5470f56da7a9b475f363
Tania <tani.02g@gmai.com>
2025-07-01 20:40:08 -0600

    Agrego Point3D.js compilado y listo para GitHub Pages

---
+++ dist/src/Point3D.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Point3D = void 0;
+var Point3D = /** @class */ (function () {
+    function Point3D(x, y, z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    return Point3D;
+}());
+exports.Point3D = Point3D;
